57eac1ae3c5a3e75d0ec37b0d7a6d58247cc4a43
khaled gabr <khaledgabr77@gmail.com>
2020-06-01 15:49:39 +0200

    configuration map launch file

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/khaled/project4_Map_My_World/src
+# Build directory: /home/khaled/project4_Map_My_World/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(my_robot)
+subdirs(teleop_twist_keyboard)
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/khaled/project4_Map_My_World/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/opt/ros/kinetic".split(';') if "/opt/ros/kinetic" != "" else []
